71fe1028dcdb3a9c92608d9758576c1baf699f89
linf8980404 <linf8980404@gmail.com>
2022-05-11 11:52:23 +0800

    1-2 使用 NPM 管理專案

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+//內建模組
+const path = require('path');
+const http = require('http');
+//第三方模組套件
+
+
+//自建模組
 const hello = require('./hello');
+
+//////////////////////////////////////////////
 
 hello.sayHello();   
 
