d395da4ce238db8d335102c457280a4c4dac866b
linf8980404 <linf8980404@gmail.com>
2022-05-11 16:34:32 +0800

    3-1 使用 Express 創建 Web Server 並設定路由 && 3-2 使用 sendFile() response HTML 檔案（搭配內建模組 path）

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 //內建模組
-const path = require('path');
-const http = require('http');
-//第三方模組套件
+const path = require('express');
 
+//第三方模組套件
+const express = require('express');
 
 //自建模組
-const hello = require('./hello');
+
 
 //////////////////////////////////////////////
 
-const server = http.createServer((req, res) => {
-	// console.log('第一個參數是瀏覽器對 web server 的 request', req);
-	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
-    console.log('req url:', req.url);
-    if(req.url === '/login'){
-        console.log('Login page')
-    } 
-	res.end();
+const app = express();
+
+app.use((req, res, next) => {
+	console.log('Hello!');
+    next();
 });
 
-server.listen(3000, () => {
-	console.log('running server on port 3000');
+app.use((req, res, next) => {
+	console.log('World!');
+    next();
 });
+
+app.get('/', (req, res) => {
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('<head><meta charset="utf-8" /></head>')
+    // res.write('<body>')
+    // res.write('<h1>這是首頁</h1>')
+    // res.write('</body>')
+    res.status(200).sendFile(path.join(__dirname, 'views', 'index.html' ));
+});
+
+app.listen(3000, () => {
+	console.log('Web Server is running on port 3000');
+});
+
