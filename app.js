53ef3fefae0825dd1c518a11642eb1ae3bff0058
linf8980404 <linf8980404@gmail.com>
2022-05-11 14:54:26 +0800

    2-1 建立 Server 並監聽變化（ 內建模組 http）

--- app.js
+++ app.js
@@ -2,28 +2,23 @@
 const path = require('path');
 const http = require('http');
 //第三方模組套件
-const cowsay = require('cowsay');
+
 
 //自建模組
 const hello = require('./hello');
 
 //////////////////////////////////////////////
 
-// hello.sayHello();   
+const server = http.createServer((req, res) => {
+	// console.log('第一個參數是瀏覽器對 web server 的 request', req);
+	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
+    console.log('req url:', req.url);
+    if(req.url === '/login'){
+        console.log('Login page')
+    } 
+	res.end();
+});
 
-// console.log(hello.title)
-
-// console.log(cowsay.say({
-//     text : "I'm a moooodule",
-//     e : "oO",
-//     T : "U "
-// }));
-
-let sentences = ['Hello', 'World', 'I\'m a cow.'];
-sentences.forEach((sentence) =>　{
-    console.log(cowsay.say({
-        text: sentence,
-        e : "00",
-        T : "U"
-    }));
+server.listen(3000, () => {
+	console.log('running server on port 3000');
 });
