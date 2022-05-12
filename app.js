7becdcf1efe7b3fcd2f0719984f17627c9f342b9
linf8980404 <linf8980404@gmail.com>
2022-05-12 10:49:36 +0800

    3-4 製作 login 頁面並處理 POST request（第三方套件 body-parser）

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 //第三方模組套件
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //自建模組
 
@@ -10,6 +11,8 @@ const express = require('express');
 //////////////////////////////////////////////
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
 	console.log('Hello!');
@@ -32,6 +35,21 @@ app.get('/login', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
+// app.post('/login', (req, res) => {
+//     const  { email, password } = req.body;
+//     console.log('Form Data', req.body);
+//     console.log('Form email', req.body);
+//     console.log('Form password', req.body);
+// });
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+        res.redirect('/');
+    } else {
+        console.log('欄位尚未填寫完成！')
+    }
+});
 app.listen(3000, () => {
 	console.log('Web Server is running on port 3000');
 });
